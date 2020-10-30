ed85292ecf122b0bbf26ad802baea4bfd4105cbb
ceez-the-day <cesar.a.villavicencio@gmail.com>
2020-10-29 21:04:41 -0400

    Added functionality to censor not just a term but a list of terms.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -10,28 +10,44 @@ email_two = open("email_two.txt", "r").read()
 #print(email_four)
 
 #defining lists
-proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+#proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
 #function replace_censor_term takes one parameter, censor_term, and censors the term from email one.
-#It will censor the terms with asterisks (*) for each character.
+#It will censor the terms with asterisks (*) for each character. It will add a space if censor term has a space.
 
-def replace_censor_term(censor_term):
-    replacing_string = ""
-    for character in censor_term:
-        replacing_string += "*"
-    censored_email_one = email_one.replace(censor_term, replacing_string)
-    return censored_email_one
-
-#print(replace_censor_term("learning algorithms"))
-
-def replace_censor_list(censor_list):
-    for term in censor_list:
-        replacing_string = ""
-        for character in term:
-            replacing_string += "*"
-            censored_email_two = email_two.replace(term, replacing_string)
-    return censored_email_two
+def censor_one(input_text, censor_term):
+    censored_item = ""
+    for i in range(0, len(censor_term)):
+        if censor_term[i] == " ":
+            censored_item += " "
+        else:
+            censored_item += "*"
+    return input_text.replace(censor_term, censored_item)
 
 
-print(replace_censor_list(proprietary_terms))
+print(censor_one(email_one, "learning algorithms"))
+
+#this was my initial try which worked but it also added an asterisk where there was a space
+#def censor_one(input_text, censor_term):
+#    censored_item = ""
+#    for i in range(0, len(censor_term)):
+#            censored_item += "*"
+#    censored_email_one = input_text.replace(censor_term, censored_item)
+#    return censored_email_one
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+def censor_two(input_text, censor_list):
+    for word in censor_list:
+        censored_word = ""
+        for i in range(0, len(word)):
+            if word[i] == " ":
+                censored_word += " "
+            else:
+                censored_word += "X"
+        input_text = input_text.replace(word, censored_word)
+    return input_text
+
+
+print(censor_two(email_two, proprietary_terms))
